73bf1175aeb9528bc40703bac025136763e37da0
nahumsvr <nahumnea@gmail.com>
2026-04-18 03:16:29 -0600

    feat: implement JWT-based authentication module with registration, login, and protected routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { PatientsModule } from './patients/patients.module';
 import { ConsultationsModule } from './consultations/consultations.module';
 import { TranscriptionsModule } from './transcriptions/transcriptions.module';
 import { UsersModule } from './users/users.module';
+import { TranscriptionModule } from './transcription/transcription.module';
+import { ExtractionModule } from './extraction/extraction.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true, // Esto hace que el módulo de configuración esté disponible en todo el proyecto
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI!),
     PatientsModule,
     ConsultationsModule,
     TranscriptionsModule,
     UsersModule,
+    TranscriptionModule,
+    ExtractionModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Habilitar validadores globalmente
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remueve propiedades que no estén en el DTO
+    forbidNonWhitelisted: true, // lanza error si hay propiedades no permitidas
+  }));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+
+  await app.listen(port);
 }
 bootstrap();
